6e1ad10996ad9d27004bf3e44559edec75f7f652
Sébastien PAQUET <cavallinox@MacBook-Pro-de-Sebastien-2.local>
2018-06-18 13:16:42 +0200

    +1

--- pages/detail-actu.php
+++ pages/detail-actu.php
@@ -1,141 +1,79 @@
 <?php	// Requête pour récupérer le contenu de la page concernée
 		list($id, $titrep, $date, $rub, $textep) = mysqli_fetch_array(mysqli_query($link, "SELECT ID, titre, dbu, rub, texte FROM ".$table_prefix."_pages WHERE page='blog' AND ID='$id' "));	
+		
+		$date = new DateTime($date);
 ?>
 
-<!-- Page Title
-		============================================= -->
-		<section id="page-title" class="page-title-parallax page-title-dark" style="position: relative; background-image: url('images/pages-don-d-organes-coeur-poumons-belgique/<?=$id?>.jpg'); padding: 120px 0; background-size: cover; background-position: center center;" data-stellar-background-ratio="0.3">
-		<div class="banner-overlay"></div>
-			<div class="container clearfix">
-				<h1><?php echo $titrep; ?></h1>
-				<ol class="breadcrumb" style="position: relative !important; left: 0 !important; margin: 30px auto 0 auto !important; ">
-					<li><a href="<?php echo $defaultpg; ?>.php">Accueil</a></li>
-					<li class="active"><?php echo $titrep; ?></li>
-				</ol>
-			</div>
+<!-- Start main-content -->
+  <div class="main-content">
+  
+    <!-- Section: inner-header -->
+    <section class="inner-header divider parallax layer-overlay overlay-dark-8" data-bg-img="'images/pages-don-d-organes-coeur-poumons-belgique/<?=$id?>.jpg'">
+      <div class="container pt-60 pb-60">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row"> 
+            <div class="col-md-12 xs-text-center">
+              <h3 class="title text-white"><?php echo $titrep; ?></h3>
+              <ol class="breadcrumb mt-10 white">
+                <li><a class="text-white" href="<?php echo $defaultpg; ?>.php">Accueil</a></li>
+                <li class="active text-theme-colored"><?php echo $titrep; ?></li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
-		</section><!-- #page-title end -->
-
-
-<!-- Content
-		============================================= -->
-		<section id="content">
-
-			<div class="content-wrap">
-
-				<div class="container clearfix">
-
-					<!-- Post Content
-					============================================= -->
-					<div class="postcontent nobottommargin clearfix">
-
-						<div class="single-post nobottommargin">
-
-							<!-- Single Post
-							============================================= -->
-							<div class="entry clearfix">
-
-								<!-- Entry Title
-								============================================= -->
-								<div class="entry-title">
-									<h2><?php echo $titrep; ?></h2>
-								</div><!-- .entry-title end -->
-
-								<!-- Entry Meta
-								============================================= -->
-								<ul class="entry-meta clearfix">
-									<li><i class="icon-calendar3"></i> <?php echo $date; ?></li>
-									
-								</ul><!-- .entry-meta end -->
-
-								<!-- Entry Image
-								============================================= -->
-								<div class="entry-image">
-									<?php if (is_file('./images/pages-don-d-organes-coeur-poumons-belgique/'.$id.'.jpg')) { ?>
-									<img  src="<?php echo './images/pages-don-d-organes-coeur-poumons-belgique/'.$id.'.jpg'; ?>" title="<?php echo $titrep; ?>" alt="<?php echo $titrep; ?>">
-									<?php } else { ?>
-										<img alt="" class="img-responsive" src="./images/photo-replace.jpg">
-									<?php } ?>
-								</div><!-- .entry-image end -->
-
-								<!-- Entry Content
-								============================================= -->
-								<div class="entry-content notopmargin">
-								
-								<?php echo $textep; ?>
-
-									<div class="clear"></div>
-									
-									<div class="line"></div>
-
-									<!-- Post Single - Share
-									============================================= -->
-									<div class="si-share noborder clearfix">
-										<span>Partagez cette actualité :</span>
-										<div>
-											<a href="http://www.facebook.com/sharer.php?u=http://<?=$_SERVER['HTTP_HOST']?><?=$_SERVER[REQUEST_URI]?>&t=<?=$titrep?>" target="_blank" class="social-icon si-small si-borderless si-facebook">
-												<i class="icon-facebook"></i>
-												<i class="icon-facebook"></i>
-											</a>
-				
-											<a href="http://twitter.com/intent/tweet/?url=<?=$_SERVER['HTTP_HOST']?><?=$_SERVER[REQUEST_URI]?>&text=<?=$titrep?>" target="_blank" class="social-icon si-small si-borderless si-twitter">
-												<i class="icon-twitter"></i>
-												<i class="icon-twitter"></i>
-											</a>
-				
-											<a href="https://plus.google.com/share?url=<?=$_SERVER['HTTP_HOST']?><?=$_SERVER[REQUEST_URI]?>&hl=<?=$titrep?>" target="_blank" class="social-icon si-small si-borderless si-gplus">
-												<i class="icon-gplus"></i>
-												<i class="icon-gplus"></i>
-											</a>
-				
-											<a href="https://pinterest.com/pin/create/button/?url=<?=$_SERVER['HTTP_HOST']?><?=$_SERVER[REQUEST_URI]?>&media=<?php echo './images/pages-don-d-organes-coeur-poumons-belgique/'.$id.'.jpg'; ?>&description=<?=$titrep?>" target="_blank" class="social-icon si-small si-borderless si-pinterest">
-												<i class="icon-pinterest"></i>
-												<i class="icon-pinterest"></i>
-											</a>
-				
-											<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?=$_SERVER['HTTP_HOST']?><?=$_SERVER[REQUEST_URI]?>&title=<?=$ogtitre?>" target="_blank" class="social-icon si-small si-borderless si-linkedin">
-												<i class="icon-linkedin"></i>
-												<i class="icon-linkedin"></i>
-											</a>
-
-										</div>
-									</div><!-- Post Single - Share End -->
-
-								</div>
-							</div><!-- .entry end -->
-
-						</div>
-
-					</div><!-- .postcontent end -->
-
-					<!-- Sidebar
-					============================================= -->
-					<div class="sidebar nobottommargin col_last clearfix">
-						<div class="sidebar-widgets-wrap">
-
-							<div class="widget clearfix">
-
-								<h4>Autres actualités</h4>
-								
-								<div class="tagcloud">
-									
-									<?php $req = mysqli_query($link,"SELECT ID, titre, dbu, rub, texte, texte2 FROM ".$table_prefix."_pages WHERE page='blog' AND ID <> ".$id." AND masquer <> '1' ORDER BY ID DESC limit 0,10"); 
-									 	while ($data = mysqli_fetch_array($req)) {
-							  		?>
-										
-											<a href="<?php echo slugify($data['titre']); ?>--<?php echo $data['ID']; ?>--detail-actu"><?php echo $data['titre']; ?></a>
-											
-									<?php } ?>
-								</div>
-								
-							</div>
-
-						</div>
-
-					</div><!-- .sidebar end -->
-
-				</div>
-
-			</div>
-
-		</section><!-- #content end -->
+    <!-- Section: Blog -->
+    <section id="blog">
+      <div class="container mt-30 mb-30 pt-30 pb-30">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="blog-posts single-post">
+              <article class="post clearfix mb-0">
+                <div class="entry-header">
+                  	<div class="post-thumb thumb">
+	                  	<?php if (is_file('./images/pages-don-d-organes-coeur-poumons-belgique/'.$id.'.jpg')) { ?>
+							<img class="img-responsive img-fullwidth" src="<?php echo './images/pages-don-d-organes-coeur-poumons-belgique/'.$id.'.jpg'; ?>" title="<?php echo $titrep; ?>" alt="<?php echo $titrep; ?>">
+						<?php } else { ?>
+							<img alt="" class="img-responsive img-fullwidth" src="./images/photo-replace.jpg">
+						<?php } ?>
+	                </div>
+                </div>
+                <div class="entry-content">
+                  <div class="entry-meta media no-bg no-border mt-15 pb-20">
+                    <div class="entry-date media-left text-center flip bg-theme-colored pt-5 pr-15 pb-5 pl-15">
+                      <ul>
+                        <li class="font-16 text-white font-weight-600"><?php echo $date->format('d'); ?></li>
+                        <li class="font-12 text-white text-uppercase"><?php echo $date->format('M'); ?></li>
+                      </ul>
+                    </div>
+                    <div class="media-body pl-15">
+                      <div class="event-content pull-left flip">
+                        <h4 class="entry-title text-white text-uppercase m-0"><a href="#"><?php echo $titrep; ?></a></h4>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="mb-15"><?php echo $textep; ?></p>
+                  <div class="mt-30 mb-0">
+                    <h5 class="pull-left mt-10 mr-20 text-theme-colored">Partager :</h5>
+                    <ul class="styled-icons icon-circled m-0">
+                      <li><a href="http://www.facebook.com/sharer.php?u=http://<?=$_SERVER['HTTP_HOST']?><?=$_SERVER[REQUEST_URI]?>&t=<?=$titrep?>" data-bg-color="#3A5795" target="_blank"><i class="fa fa-facebook text-white"></i></a></li>
+                      <li><a href="http://twitter.com/intent/tweet/?url=<?=$_SERVER['HTTP_HOST']?><?=$_SERVER[REQUEST_URI]?>&text=<?=$titrep?>" data-bg-color="#55ACEE" target="_blank"><i class="fa fa-twitter text-white"></i></a></li>
+                      <li><a href="https://www.linkedin.com/shareArticle?mini=true&url=<?=$_SERVER['HTTP_HOST']?><?=$_SERVER[REQUEST_URI]?>&title=<?=$ogtitre?>" data-bg-color="#A11312" target="_blank"><i class="fa fa-linkedin text-white"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </article>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <!-- end main-content -->
+  
+  
+    
